1cad0f8380a8f8186e43d2c700b871b30c446fd7
Akash <technicalak282@gmail.com>
2024-03-08 15:38:00 +0530

    Design:Desktop is done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import FormCard from "./components/FormCard";
+import FormCardDesktop from "./components/FormCardDesktop";
 
 export default function App() {
   return (
-    <main className="w-full min-h-screen *:font-UbuntuFont bg-LightBlue flex justify-center p-8 items-center">
-      <FormCard />
+    <main className="max-w-[1440px] min-w-[375px] w-full min-h-screen *:font-UbuntuFont bg-LightBlue flex justify-center p-8 items-center">
+      <FormCardDesktop />
     </main>
   );
 }
